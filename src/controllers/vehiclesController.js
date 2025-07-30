53e1806f2037b1969bfffd99523c3409634335be
Viviane Silva <viviane.silva1810@gmail.com>
2025-07-30 00:02:56 +0000

    fac: remoção de redundancia no código

--- src/controllers/vehiclesController.js
+++ src/controllers/vehiclesController.js
@@ -7,7 +7,7 @@ export const getVehicles = async (req, res) => {
                 id: 1
             }
         })
-        res.status(200).json(result.rows);
+        res.status(200).json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).json({ error: "Erro ao buscar veículos" });
